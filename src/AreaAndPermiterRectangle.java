862abdd7c9ecbf4ec943d5fd5d3833e6e09c55d3
steven-halla <69405488+steven-halla@users.noreply.github.com>
2023-04-11 20:46:58 -0700

    area of a rectangle

---
+++ src/AreaAndPermiterRectangle.java
@@ -0,0 +1,8 @@
+public class AreaAndPermiterRectangle {
+    public static void main(String[] args) {
+        double rectangleWidth = 4.5;
+        double rectangleHeight = 7.9;
+        double rectangleArea = rectangleHeight * rectangleWidth;
+        System.out.println("area of the rectangle is:  " + rectangleArea);
+    }
+}
