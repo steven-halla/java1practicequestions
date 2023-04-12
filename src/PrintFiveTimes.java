594af6f7698f82975312a028ed0fda0366ed5c6c
steven-halla <69405488+steven-halla@users.noreply.github.com>
2023-04-11 19:32:39 -0700

    first

---
+++ src/PrintFiveTimes.java
@@ -0,0 +1,10 @@
+public class PrintFiveTimes {
+    public static void main(String[] args) {
+        String welcomeMesage = "welcome to java";
+
+        for( int i = 0; i  <  5; i++) {
+            System.out.println(welcomeMesage);
+        }
+
+    }
+}
--- src/TriplePrint.java
+++ src/TriplePrint.java
@@ -1,2 +1,9 @@
+
 public class TriplePrint {
+    public static void main(String[] args) {
+        System.out.println("welcome to java");
+        System.out.println("welcome to computer science");
+        System.out.println("programming is fun");
+    }
 }
+
