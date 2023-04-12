6e841a35c0e8a621abb9c260befa947468f2d473
steven-halla <69405488+steven-halla@users.noreply.github.com>
2023-04-11 19:54:32 -0700

    JAVA spelled out in characters

---
+++ src/PowersOfANumber.java
@@ -0,0 +1,9 @@
+public class PowersOfANumber {
+    public static void main(String[] args) {
+        System.out.println("A  A^2  A^3");
+        System.out.println("1  1     1");
+        System.out.println("2  4     8");
+        System.out.println("3  9     27");
+        System.out.println("4  16    64");
+    }
+}
