dea0fc995553d0f70383890e67a38ff6f332a08d
steven-halla <69405488+steven-halla@users.noreply.github.com>
2023-04-11 20:44:48 -0700

    area and permiter of a circle

---
+++ src/AreaAndPerimeterCircle.java
@@ -0,0 +1,11 @@
+public class AreaAndPerimeterCircle {
+    public static void main(String[] args) {
+        double radius = 5.5;
+        double pie = Math.PI;
+        double perimeter = 2 * radius * pie;
+        double area = radius * radius * pie;
+        System.out.println("the area of the circle is: " + area);
+        System.out.println("the permiter of the circle is: " + perimeter);
+    }
+}
+
