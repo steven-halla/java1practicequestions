78ab892528808328fd94c320a049b5e5a3cfc0f9
steven-halla <69405488+steven-halla@users.noreply.github.com>
2023-04-11 20:35:31 -0700

    1.7 finished....my cat is sleeping next to my keyboard....

---
+++ src/addingNumbers.java
@@ -0,0 +1,6 @@
+public class addingNumbers {
+    public static void main(String[] args) {
+        double addedNumbers = 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9;
+        System.out.println(addedNumbers);
+    }
+}
