6f1ef050a1a3272ffa0adc3cafc7354cd33c7dad
steven-halla <69405488+steven-halla@users.noreply.github.com>
2023-04-12 17:15:20 -0700

    feet to meters

--- src/chapter2/FeetToMeters.java
+++ src/chapter2/FeetToMeters.java
@@ -1,4 +1,10 @@
 package chapter2;
 
 public class FeetToMeters {
+    public static void main(String[] args) {
+
+        double feet = 16.5;
+        double meters = feet * .305;
+        System.out.println(feet + "Feet is " + meters + " meters long");
+    }
 }
