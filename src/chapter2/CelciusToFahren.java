033f160d4db8b021c5dfd22210a08518c5511622
steven-halla <69405488+steven-halla@users.noreply.github.com>
2023-04-12 17:07:40 -0700

    cel to fah converter created

--- src/chapter2/CelciusToFahren.java
+++ src/chapter2/CelciusToFahren.java
@@ -1,4 +1,15 @@
 package chapter2;
 
+import org.w3c.dom.ls.LSOutput;
+
 public class CelciusToFahren {
+    public static void main(String[] args) {
+        double celsiusDegrees = 43;
+        double fahrenheitDegrees = (9.0/5.0) * celsiusDegrees + 32;
+
+        System.out.println(fahrenheitDegrees);
+    }
+
+
+
 }
