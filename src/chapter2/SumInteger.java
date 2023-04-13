033fd3f98156ee39ca2857740f8d6fa2055766c9
steven-halla <69405488+steven-halla@users.noreply.github.com>
2023-04-13 15:30:19 -0700

    sum int

--- src/chapter2/SumInteger.java
+++ src/chapter2/SumInteger.java
@@ -1,4 +1,24 @@
 package chapter2;
+import java.util.Scanner;
 
-public class SumInteger s{
+
+import java.util.Scanner;
+
+public class SumInteger {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter an integer between 0 and 1000: ");
+        int num = scanner.nextInt();
+
+        int sum = 0;
+        while (num > 0) {
+            sum += num % 10;  // add the last digit to the sum
+            num /= 10;       // remove the last digit from the number
+        }
+
+        System.out.println("The sum of the digits is " + sum);
+
+        scanner.close();
+    }
 }
