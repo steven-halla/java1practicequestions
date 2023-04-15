9f12e7d44cf0cb8dc21cbc635d401949ee9590c9
steven-halla <69405488+steven-halla@users.noreply.github.com>
2023-04-15 16:09:54 -0700

    non decreating order

---
+++ src/chapter3/NonDecreasingOrder.java
@@ -0,0 +1,20 @@
+package chapter3;
+
+//inputs needed so scanner class
+
+// we need 3 inputs, so that means 3 vars
+
+// we dont want to
+
+import java.util.Scanner;
+
+public class NonDecreasingOrder {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter value of a: ");
+        double a = input.nextDouble();
+
+
+    }
+}
