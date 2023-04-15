e8c3ebac878c8f07ba5b98159f85366fa5791b24
steven-halla <69405488+steven-halla@users.noreply.github.com>
2023-04-15 16:24:30 -0700

    non decreating order

--- src/chapter3/NonDecreasingOrder.java
+++ src/chapter3/NonDecreasingOrder.java
@@ -1,20 +1,45 @@
-package chapter3;
-
-//inputs needed so scanner class
-
-// we need 3 inputs, so that means 3 vars
-
-// we dont want to
-
 import java.util.Scanner;
 
 public class NonDecreasingOrder {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        System.out.print("Enter value of a: ");
-        double a = input.nextDouble();
+        System.out.println("Enter three integers:");
+        int int1 = input.nextInt();
+        int int2 = input.nextInt();
+        int int3 = input.nextInt();
 
+        int leastAmount, middleAmount, mostAmount;
 
+        if (int1 <= int2 && int1 <= int3) {
+            leastAmount = int1;
+            if (int2 <= int3) {
+                middleAmount = int2;
+                mostAmount = int3;
+            } else {
+                middleAmount = int3;
+                mostAmount = int2;
+            }
+        } else if (int2 <= int1 && int2 <= int3) {
+            leastAmount = int2;
+            if (int1 <= int3) {
+                middleAmount = int1;
+                mostAmount = int3;
+            } else {
+                middleAmount = int3;
+                mostAmount = int1;
+            }
+        } else {
+            leastAmount = int3;
+            if (int1 <= int2) {
+                middleAmount = int1;
+                mostAmount = int2;
+            } else {
+                middleAmount = int2;
+                mostAmount = int1;
+            }
+        }
+
+        System.out.println("Non-decreasing order: " + leastAmount + " " + middleAmount + " " + mostAmount);
     }
 }
